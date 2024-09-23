d14b0f246a8abd845d18ab847db1dc7931dba875
Keyan <karthikeyan@centizen.com>
2024-09-23 18:36:11 +0530

    base commit

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { LogIn, PenBox } from "lucide-react"
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import UserMenu from "./UserMenu";
+const Header = () => {
+    return (
+        <nav className="mx-auto py-1 px-4 flex justify-between items-center shadow-md border-b-2">
+            <Link href={"/"} className="flex items-center">
+                <Image alt="logo" src={'/logo.png'} width={80} height={30} className="h-16 w-auto" />
+            </Link>
+            <div className="flex items-center gap-3">
+                <Link href={"/events?create=true"}>
+                    <Button className="flex items-center gap-2"><PenBox size="18" />Create Event</Button>
+                </Link>
+                <SignedOut>
+                    <SignInButton forceRedirectUrl="/dashboard">
+                        <Button className="flex items-center justify-center gap-2" variant="outline"><LogIn size="18" />Login</Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserMenu />
+                </SignedIn>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Header;
---
+++ components/UserMenu.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { UserButton } from "@clerk/nextjs"
+import { ChartGanttIcon } from "lucide-react"
+
+const UserMenu = () => {
+    return (<UserButton
+        appearance={{
+            elements: {
+                avatarBox: "w-10 h-10"
+            }
+        }} >
+        <UserButton.MenuItems>
+            <UserButton.Link label="My Events" labelIcon={<ChartGanttIcon size={18} />} href="/events" />
+            <UserButton.Action label="manageAccount" />
+        </UserButton.MenuItems>
+    </UserButton >
+    )
+}
+
+export default UserMenu
