64d5d450aeac29c1e8804ac40060d47ca5ff83a4
Keyan <karthikeyan@centizen.com>
2024-09-24 18:46:49 +0530

    dashboard design

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,6 +1,41 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useUser } from "@clerk/nextjs"
+
 const Dashboard = () => {
+    const { isLoaded, user } = useUser();
+    console.log(user);
     return (
-        <div>dashboard</div>
+        <div className="space-y-8">
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        Welcome, {user?.firstName}
+                    </CardTitle>
+                </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        Your Unique Link
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form className="space-y-4">
+                        <div>
+                            <div className="flex items-center gap-2">
+                                <span>{window.location.origin}/</span>
+                                <Input placeholder="username" />
+                            </div>
+                        </div>
+                        <Button>Update username</Button>
+                    </form>
+                </CardContent>
+            </Card>
+
+        </div>
     )
 }
 
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@
 import { useUser } from "@clerk/nextjs"
 import { BarChart, Calendar, Clock, Users } from "lucide-react";
 import Link from "next/link"
+import { usePathname } from "next/navigation";
 import { BarLoader } from "react-spinners";
 const navItems = [
     { href: "/dashboard", label: "Dashboard", icon: BarChart },
@@ -11,21 +12,41 @@ const navItems = [
 ];
 const AppLayout = ({ children }) => {
     const { isLoaded } = useUser();
+    const pathName = usePathname();
     return (<>
         {!isLoaded && <BarLoader width="100%"></BarLoader>}
-        <div>
-            <aside>
-                <nav>
+        <div className="flex flex-col h-screen bg-blue-50 md:flex-row">
+            <aside className="hidden md:block w-64 bg-white">
+                <nav className="mt-5">
                     {navItems.map((item) => (
-                        <Link href={item.href} key={item.href} className="flex items-center px-4 py-7 text-gray-700 hover:bg-gray-100">
+                        <Link href={item.href} key={item.href} className={`flex items-center px-4 py-5 text-gray-700 hover:bg-gray-100 text-xl ${pathName == item.href ? 'bg-blue-100' : ''}`}>
+                            <item.icon className="w-5 h-5 mr-3"></item.icon>
                             {item.label}
-                            <item.icon></item.icon>
                         </Link>
                     ))}
                 </nav>
             </aside>
+            <main className="flex-1 overflow-y-auto p-4 md:p-8">
+                <header className="flex justify-between items-center mb-4">
+                    <h2 className="text-3xl md:text-4xl gradient-title pt-2 md:pt-0 text-center md:text-left w-full">
+                        {navItems.find((item) => item.href == pathName)?.label || 'Dashboard '}
+                    </h2>
+                </header>
+                {children}
+            </main>
+            <nav className=" md:hidden fixed bottom-0 left-0 right-0 bg-white shadow-md">
+                <ul className="flex justify-around">
+                    {navItems.map((item) => (
+                        <li key={item.href}>
+                            <Link href={item.href} className={`flex flex-col items-center px-4 py-5 ${pathName == item.href ? 'text-blue-600' : 'text-gray-600'}`}>
+                                <item.icon className="w-5 h-5"></item.icon>
+                                {item.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </div>
-        {children}
     </>)
 }
 export default AppLayout
