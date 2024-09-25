84f98a7ca70447ba896cb041abbc5fb5276794ae
Keyan <karthikeyan@centizen.com>
2024-09-25 11:11:26 +0530

    not found

---
+++ app/_not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="text-2xl md:text-4xl font-extrabold w-full pt-80 grid place-items-center">
+            <h1>404 - Page not Found</h1>
+        </div>
+    )
+}
